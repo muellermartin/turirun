f73d41a173a85dd0b312b3dad8775aebd7e7e0ef
Philipp Kübler <info@pkuebler.de>
2015-11-06 22:49:49 +0100

    First try

--- core/src/net/mueller_martin/turirun/Turirun.java
+++ core/src/net/mueller_martin/turirun/Turirun.java
@@ -1,27 +1,22 @@
 package net.mueller_martin.turirun;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Turirun extends ApplicationAdapter {
+public class Turirun extends Game {
 	SpriteBatch batch;
-	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		// Load Settings
+		// Load Assets
+
+		setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 }
