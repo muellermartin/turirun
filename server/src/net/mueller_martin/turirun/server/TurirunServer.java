006719cde3ccbf571498d37850829e5fd48ef2db
Martin Müller <mail@mueller-martin.net>
2015-11-07 03:04:31 +0100

    Use network library in server as dependency

--- server/src/net/mueller_martin/turirun/server/TurirunServer.java
+++ server/src/net/mueller_martin/turirun/server/TurirunServer.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 
 import com.esotericsoftware.kryonet.Server;
 
+import net.mueller_martin.turirun.network.TurirunNetwork;
+
 public class TurirunServer {
 	public static void main(String[] args) {
 		try {
 			Server server = new Server();
 
 			server.start();
-			server.bind(1337, 7331);
+			server.bind(TurirunNetwork.tcpPort, TurirunNetwork.udpPort);
 		}
 		catch (IOException e) {
 			// TO DO: Fail
