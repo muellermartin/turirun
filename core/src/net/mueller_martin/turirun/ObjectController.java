ce34a92478e004994469c69ad3932015aba4e58a
Martin Müller <mail@mueller-martin.net>
2015-11-07 13:30:31 +0100

    Revert "Remove id attributes from ObjectController"
    This reverts commit 442b714f29e44488cddb043537cdfa81f76bfca8.

--- core/src/net/mueller_martin/turirun/ObjectController.java
+++ core/src/net/mueller_martin/turirun/ObjectController.java
@@ -8,17 +8,31 @@ import net.mueller_martin.turirun.gameobjects.GameObject;
  * Created by DM on 06.11.15.
  */
 public class ObjectController {
+
+	private static int next_id = 0;
+	private HashMap<Integer,GameObject> objs_map = new HashMap<Integer,GameObject>();
 	private ArrayList<GameObject> objs = new ArrayList<GameObject>();
 
 	public void addObject(GameObject obj) {
+		this.next_id++;
+		
+		int id = next_id;
+		obj.id = next_id;
+
+		objs_map.put(id, obj);
 		objs.add(obj);
 	}
 
 	public void removeObject(GameObject obj) {
+		objs_map.remove(obj.id);
 		objs.remove(obj);
 	}
 
 	public ArrayList<GameObject> getObjects() {
 		return objs;
+	}
+
+	public GameObject getObject(int id) {
+		return objs_map.get(id);
 	}
 }
