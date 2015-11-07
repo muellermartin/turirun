e8d7ea7e51888059e5dfe1d256732901a780d6b4
Martin Müller <mail@mueller-martin.net>
2015-11-07 11:10:18 +0100

    Set clear color to black

--- core/src/net/mueller_martin/turirun/screens/GameScreen.java
+++ core/src/net/mueller_martin/turirun/screens/GameScreen.java
@@ -19,7 +19,7 @@ public class GameScreen extends ScreenAdapter {
 	}
 
 	public void draw() {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		world.draw(game.batch);
 	}
