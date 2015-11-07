3fbbc848be7af015cbdf51343bc4db1a4ca1abb9
Philipp Kübler <info@pkuebler.de>
2015-11-07 10:46:17 +0100

    Add Character Object

---
+++ core/src/net/mueller_martin/turirun/gameobjects/CharacterObject.java
@@ -0,0 +1,13 @@
+package net.mueller_martin.turirun.gameobjects;
+
+import net.mueller_martin.turirun.gameobjects.GameObject;
+/**
+ * Created by DM on 06.11.15.
+ */
+public class CharacterObject extends GameObject {
+	public String username = "Gast";
+
+	public CharacterObject (float x, float y, float width, float height) {
+		super(x,y,width,height);
+	}
+}
