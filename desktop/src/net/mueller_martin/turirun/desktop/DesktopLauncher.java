9c705c449fd5cab4a995b94bde8cabd33151e231
DvsZ <dvzedlitz@arcor.de>
2015-11-07 11:16:18 +0100

    Map bonds collusion

--- desktop/src/net/mueller_martin/turirun/desktop/DesktopLauncher.java
+++ desktop/src/net/mueller_martin/turirun/desktop/DesktopLauncher.java
@@ -2,11 +2,14 @@ package net.mueller_martin.turirun.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.mueller_martin.turirun.Constants;
 import net.mueller_martin.turirun.Turirun;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Constants.VIEWPORTWIDTH;
+		config.height= Constants.VIEWPORTHEIGHT;
 		new LwjglApplication(new Turirun(), config);
 	}
 }
