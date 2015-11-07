916e2a2b661d127b1300db18a2fb2aeaba46b26f
Martin Müller <mail@mueller-martin.net>
2015-11-07 02:11:18 +0100

    Use kryonet for network

--- server/src/net/mueller_martin/turirun/server/TurirunServer.java
+++ server/src/net/mueller_martin/turirun/server/TurirunServer.java
@@ -1,8 +1,8 @@
 package net.mueller_martin.turirun.server;
 
-//import com.esotericsoftware.kryonet;
+import com.esotericsoftware.kryonet.Server;
 
-public class Server {
+public class TurirunServer {
 	public static void main(String[] args) {
 		System.out.println("Hello, world");
 	}
