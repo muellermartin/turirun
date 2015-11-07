49906219bd10a7919b9e058e9cc26857788f5dc1
Martin Müller <mail@mueller-martin.net>
2015-11-07 02:33:38 +0100

    Implement simple TCP/UDP server with kryonet

--- server/src/net/mueller_martin/turirun/server/TurirunServer.java
+++ server/src/net/mueller_martin/turirun/server/TurirunServer.java
@@ -1,9 +1,19 @@
 package net.mueller_martin.turirun.server;
 
+import java.io.IOException;
+
 import com.esotericsoftware.kryonet.Server;
 
 public class TurirunServer {
 	public static void main(String[] args) {
-		System.out.println("Hello, world");
+		try {
+			Server server = new Server();
+
+			server.start();
+			server.bind(1337, 7331);
+		}
+		catch (IOException e) {
+			// TO DO: Fail
+		}
 	}
 }
