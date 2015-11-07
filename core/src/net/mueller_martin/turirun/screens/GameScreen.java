5e4ff217243aec763158ff69ecf1b0ed99d99e10
Martin Müller <mail@mueller-martin.net>
2015-11-07 04:02:38 +0100

    Removed demo graphics

--- core/src/net/mueller_martin/turirun/screens/GameScreen.java
+++ core/src/net/mueller_martin/turirun/screens/GameScreen.java
@@ -2,7 +2,6 @@ package net.mueller_martin.turirun.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Rectangle;
@@ -14,21 +13,14 @@ public class GameScreen extends ScreenAdapter {
 	Turirun game;
 	WorldController world;
 
-	Texture img;
-
 	public GameScreen (Turirun game) {
 		this.game = game;
 		this.world = new WorldController(game);
-
-		img = new Texture("badlogic.jpg");
 	}
 
 	public void draw() {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		game.batch.begin();
-		game.batch.draw(img, 0, 0);
-		game.batch.end();
 		world.draw(game.batch);
 	}
 
