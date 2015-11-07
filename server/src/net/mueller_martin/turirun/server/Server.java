79bc843f3ae1fb36fc33bcf3dc13fd70b7538b96
Martin Müller <mail@mueller-martin.net>
2015-11-07 02:04:47 +0100

    Add server project

---
+++ server/src/net/mueller_martin/turirun/server/Server.java
@@ -0,0 +1,9 @@
+package net.mueller_martin.turirun.server;
+
+//import com.esotericsoftware.kryonet;
+
+public class Server {
+	public static void main(String[] args) {
+		System.out.println("Hello, world");
+	}
+}
