ad47dfed2c4d8dfad4e0eeb1c74191af0655488e
Philipp Kübler <info@pkuebler.de>
2015-11-07 02:16:36 +0100

    Draw Blue Rectangle as GameObject

--- core/src/net/mueller_martin/turirun/WorldController.java
+++ core/src/net/mueller_martin/turirun/WorldController.java
@@ -3,6 +3,7 @@ package net.mueller_martin.turirun;
 import net.mueller_martin.turirun.gameobjects.GameObject;
 import net.mueller_martin.turirun.gameobjects.DynamicGameObject;
 import net.mueller_martin.turirun.CharacterController;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 /**
  * Created by DM on 06.11.15.
  *
@@ -31,13 +32,16 @@ public class WorldController {
 
     // Start Game
     public void init() {
+        GameObject obj = new GameObject(100, 100, 20, 20);
+        this.objs.addObject(obj);
+
 //        DynamicGameObject playerObj = new DynamicGameObject();
 //        controller.setPlayerObj(playerObj);
     }
 
-    public void draw() {
+    public void draw(SpriteBatch batch) {
     	for (GameObject obj: objs.getObjects()) {
-    		obj.draw();
+    		obj.draw(batch);
     	}
     }
 
